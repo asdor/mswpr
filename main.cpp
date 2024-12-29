54b84703bc22b5875929bd1bd59f09d62ffcca4e
asdor <asdor1812@gmail.com>
2024-12-30 00:28:48 +0100

    Catch exceptions from game loop

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include "gui/game_engine.hpp"
 
+#include <iostream>
+
 int main()
 {
-  mswpr::game_engine engine("mswpr", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-
-  while (engine.running())
+  try
   {
-    engine.handle_input();
-    engine.update();
-    engine.render();
+    mswpr::game_engine engine("mswpr", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
-    engine.limit_fps();
+    while (engine.running())
+    {
+      engine.handle_input();
+      engine.update();
+      engine.render();
+
+      engine.limit_fps();
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "The game was terminated with exception: " << e.what() << '\n';
+    return -1;
   }
 
   return 0;
