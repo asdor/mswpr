9a824c44975d590741ab9656c33a2339b426126b
asdor <asdor1812@gmail.com>
2025-01-25 02:22:12 +0100

    [#15] Fix compile errors with logger

--- src/gui/logger.cpp
+++ src/gui/logger.cpp
@@ -10,10 +10,11 @@ namespace
 {
   constexpr auto MSWPR_LOGGER_NAME = "mswpr";
 
-  std::shared_ptr<spdlog::logger> initialize_logger()
+  std::shared_ptr<spdlog::logger> initialize_mswpr_logger()
   {
-    const std::filesystem::path log_file = SDL_GetBasePath();
-    auto logger = spdlog::basic_logger_st(MSWPR_LOGGER_NAME, log_file / "sdl2_minesweeper.log");
+    const std::filesystem::path path_to_binary = SDL_GetBasePath();
+    const std::filesystem::path log_file = path_to_binary / "sdl2_minesweeper.log";
+    auto logger = spdlog::basic_logger_st(MSWPR_LOGGER_NAME, log_file.string());
     spdlog::set_level(spdlog::level::debug);
     return logger;
   }
@@ -23,6 +24,6 @@ std::shared_ptr<spdlog::logger> mswpr::get_mswpr_logger()
 {
   auto logger = spdlog::get(MSWPR_LOGGER_NAME);
   if (!logger)
-    return ::initialize_logger();
+    return ::initialize_mswpr_logger();
   return logger;
 }
